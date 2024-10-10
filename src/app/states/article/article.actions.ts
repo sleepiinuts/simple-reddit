9339f22e8ea4be18f513c51e4a45c88a268605af
sleepynut <nut.pa@kbtg.tech>
2024-10-10 19:42:43 +0700

    trying ngrx

---
+++ src/app/states/article/article.actions.ts
@@ -0,0 +1,16 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { Article } from '../../models/articles';
+
+export const ArticleActions = createActionGroup({
+  source: 'Article',
+  events: {
+    'Get All': emptyProps(),
+    'Get All Success': props<{ data: Article[] }>(),
+    'Get All Failure': props<{ error: unknown }>(),
+  },
+});
+
+// get all
+// new article
+// upvote
+// downvote
---
+++ src/app/states/article/article.reducer.ts
@@ -0,0 +1,21 @@
+import { createReducer, on } from '@ngrx/store';
+import { ArticleActions } from './article.actions';
+import { Article } from '../../models/articles';
+
+export const articleFeatureKey = 'article';
+
+export interface State {
+  articles: Article[];
+}
+
+export const initialState: State = {
+  articles: [],
+};
+
+export const reducer = createReducer(
+  initialState,
+  on(ArticleActions.getAllSuccess, (state, { data }) => ({
+    ...state,
+    articles: [...data],
+  }))
+);
