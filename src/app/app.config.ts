2289ce92dd75623c72d30c16840f810353efcc72
sleepynut <nut.pa@kbtg.tech>
2024-10-10 19:43:12 +0700

    misc

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
+import { provideState } from '@ngrx/store';
+import { articleFeatureKey, reducer } from './states/article/article.reducer';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideStore()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideStore(),
+    provideState({ name: articleFeatureKey, reducer: reducer }),
+    provideRouterStore(),
+    provideHttpClient(),
+  ],
 };
